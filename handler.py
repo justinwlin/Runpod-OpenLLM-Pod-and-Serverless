7089055d51a3af99935b05333dd6546a2f2bac6d
Justin Lin <justinwlin@gmail.com>
2024-03-03 22:58:04 -0500

    Adding options to handler pass through

--- handler.py
+++ handler.py
@@ -2,6 +2,10 @@ import os
 import openllm
 import runpod
 import asyncio
+import torch
+import numpy as np
+import random
+import uuid
 
 # Use the MODEL environment variable; fallback to a default if not set
 model_name = os.getenv("MODEL", "mistralai/Mistral-7B-Instruct-v0.1")
@@ -26,7 +30,9 @@ except:
 async def handler(event):
     inputReq = event.get("input", {})
     user_prompt = inputReq.get("prompt")
-    answerType = inputReq.get("answerType", "normal").lower()  # Ensure lowercase for comparison
+    answerType = inputReq.get("answerType", "normal").lower()
+    # Retrieve temperature from the event, defaulting to 0.7 if not provided.
+    options = inputReq.get("options", {})
 
     if answerType not in ["stream", "normal"]:
         print("answerType not correctly set, defaulting to stream")
@@ -35,14 +41,17 @@ async def handler(event):
     if not user_prompt:
         yield {"error": "No user prompt provided"}
         return
+    print("LLM: ", llm)
+    print("Options: ", options)
 
     try:
         if answerType == "stream":
-            async for generation in llm.generate_iterator(user_prompt):
+            async for generation in llm.generate_iterator(user_prompt, **options):
                 yield {"text": generation.outputs[0].text}
         elif answerType == "normal":
-            response = await llm.generate(user_prompt)
-            yield {"text": response.outputs[0].text}  # Ensure consistent response format
+            print("Generating response:...")
+            response = await llm.generate(user_prompt, **options)  # Pass temperature and other options here.
+            yield {"text": response.outputs[0].text}
     except Exception as e:
         yield {"error": str(e)}
 
@@ -58,7 +67,11 @@ if mode_to_run in ["both", "serverless"]:
 
 if mode_to_run == "pod":
     async def main():
-        async for response in handler({"input": {"prompt": "Hello world", "answerType": "normal"}}):
+        randomness = str(uuid.uuid4())
+        prompt = f"[INST] You are a helpful code assistant. Your task is to generate a valid JSON object. The JSON object will contain only a single key 'idea [/INST]"
+        requestObject = {"input": {"prompt": prompt, "answerType": "normal", "options": { "temperature": 0.8, "top_k": 1000}}}
+        
+        async for response in handler(requestObject):
             print(response)
 
     asyncio.run(main())
